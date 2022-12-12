0e1aa3d901c838abf512d3c7d53c5f00dbee2719
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-12 15:14:06 +0100

    PHP: Paragrafo e lunghezza

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
 
+$paragrafoC20 =
+'Fui catapultato in un mondo completamente nullo, privo di terra, alberi o montagne.
+Una dimensione inesistente dove potevo scorgere solo uno sfondo bianco, null’altro.
+Fui costretto ad agitare le ali per non essere vittima della gravità,
+ma sapevo bene che ad un certo punto avrei finito le energie e sarei crollato… 
+“Che diavolo di posto è? Che cosa devo fare? Qui non c’è assolutamente niente…” ';
+
+echo $paragrafoC20;
+
+echo strlen($paragrafoC20)
 
 
 ?>
